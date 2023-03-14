eee91a97fb27e8e868e7deaf9a684c1e0e8c860e
Eli <69530035+elisvcodes@users.noreply.github.com>
2023-03-14 19:45:41 -0400

    added the routes & parser to app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,12 @@
 import express from "express";
+import userRoute from "./routes/user";
+import recipeRoute from "./routes/recipe";
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRoute);
+app.use("/recipe", recipeRoute);
 
 app.listen(8000);
