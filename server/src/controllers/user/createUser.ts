ab1e1a302bd477a4c61169c09692e2899dc3836d
Eli <69530035+elisvcodes@users.noreply.github.com>
2023-03-14 19:52:02 -0400

    finished the create user

--- server/src/controllers/user/createUser.ts
+++ server/src/controllers/user/createUser.ts
@@ -3,13 +3,20 @@ import { hashPassword } from "../../util/passwords";
 import prismaClient from "../../util/prismaClient";
 
 const createUser = async (req: Request, res: Response) => {
-  const user = await prismaClient.user.create({
-    data: {
-      name: "",
-      email: "",
-      password: await hashPassword(""),
-    },
-  });
+  const { name, email, password } = req.body;
+
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: await hashPassword(password),
+      },
+    });
+    res.status(200).json("success");
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 export default createUser;
